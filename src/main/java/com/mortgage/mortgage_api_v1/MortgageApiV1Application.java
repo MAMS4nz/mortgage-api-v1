705b7363a08340e12d06e45c7a309819c5bfb6d7
MAMS4nz <132152058+MAMS4nz@users.noreply.github.com>
2025-01-11 20:52:35 +0100

    Initial commit

---
+++ src/main/java/com/mortgage/mortgage_api_v1/MortgageApiV1Application.java
@@ -0,0 +1,13 @@
+package com.mortgage.mortgage_api_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageApiV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageApiV1Application.class, args);
+	}
+
+}
---
+++ src/main/resources/datastores/rdbmss/h2/scripts/schema.sql
@@ -0,0 +1,11 @@
+-- Creation of the mortgage rates table.
+
+CREATE TABLE mortgage_rates (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    maturity_period_months INT NOT NULL UNIQUE,
+    yearly_interest_percentage_rate DECIMAL(10, 4) NOT NULL, -- DECIMAL in order to be financially precise.
+    created DATETIME NOT NULL,
+    created_by NVARCHAR(60) NOT NULL,
+    last_updated DATETIME NOT NULL,
+    last_updated_by NVARCHAR(60) NOT NULL
+);
